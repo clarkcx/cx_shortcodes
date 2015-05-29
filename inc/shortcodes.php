2885e770fbf98820694820c3f9f74a2c5a597d0b
Pete Clark <pete@ablewild.com>
2015-05-29 13:48:41 +0100

    Version 1.0

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -217,10 +217,10 @@ add_shortcode('vimeo', 'cx_sc_responsive_video_vimeo');
 function cx_sc_responsive_video_vimeo($atts, $content = null) {
 	extract(shortcode_atts(array(
 		"id" => '',
-		"class" => 'cx_video'
+		"class" => 'videoWrapper'
 	), $atts));
 	
-	$video = '<div class="cx_video" style="position: relative; width: 100%; height: 0px; padding-bottom: 60%;">';
+	$video = '<div class="videoWrapper" style="position: relative; width: 100%; height: 0px; padding-bottom: 56.25%; margin-top: 2em; margin-bottom: 2em;">';
 	$video .= '<iframe style="position: absolute; left: 0px; top: 0px; width: 100%; height: 100%" src="//player.vimeo.com/video/'.$id.'">';
 	$video .= '</iframe>';
 	$video .= '</div>';
